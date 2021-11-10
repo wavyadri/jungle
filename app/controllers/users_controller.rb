43d3af636987567f350152f07dc33e19b7ecef23
Adriana <adriana.calvomatos@gmail.com>
2021-11-10 21:31:40 +0000

    style signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,19 +2,17 @@ class UsersController < ApplicationController
   def new
   end
 
-  def create
-    @user = User.new(user_params)
 
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        session[:user_id] = user.id
-        redirect_to '/'
-      else
-        redirect_to '/signup'
-      end
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
     end
-  end 
+  end
+  
 
   private
 
