4a23e37619925a0bebf19e0a68c0946b5e8a787c
Adriana <adriana.calvomatos@gmail.com>
2021-11-10 19:06:57 +0000

    refactor create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,21 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      redirect_to [:admin, :users], notice: 'User created!'
-    else
-      render :new
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = user.id
+        redirect_to '/'
+      else
+        redirect_to '/signup'
+      end
     end
   end 
 
   private
 
   def user_params
-    params.require(:product).permit(
+    params.require(:user).permit(
       :first_name,
       :last_name,
       :email,
