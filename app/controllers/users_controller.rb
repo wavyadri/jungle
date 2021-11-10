2282dcdd684264e9d7685842da9d15ad892ae748
Adriana <adriana.calvomatos@gmail.com>
2021-11-10 19:05:17 +0000

    update create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to [:admin, :users], notice: 'User created!'
+    else
+      render :new
+    end
+  end 
+
+  private
+
+  def user_params
+    params.require(:product).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation,
+    )
+  end
 end
