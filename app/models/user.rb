7ca30d0b6b409c7ac6c85f2597c0660cf7334762
Adriana <adriana.calvomatos@gmail.com>
2021-11-13 03:20:28 +0000

    add validation specs for each form field

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email,                 presence: true, uniqueness: { case_sensitive: false }
-  validates :password,              length: { minimum: 3 }
+  validates :password,              presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
   validates :first_name,            presence: true
   validates :last_name,             presence: true
@@ -15,5 +15,5 @@ class User < ActiveRecord::Base
       return nil
     end
   end
-  
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   describe 'Validations' do
-    # validation examples here
+    it "is a valid user" do
+      @user = User.new(:first_name => "Adri", :last_name => "Ana", :email => "adriana@gmail.com", :password => "123456789", :password_confirmation => "123456789")
+      expect(@user).to be_valid
+    end
+
+    it  "is invalid without a first name" do
+      @user = User.new(:first_name => nil, :last_name => "Ana", :email => "adriana@gmail.com", :password => "123456789", :password_confirmation => "123456789")
+      expect(@user).to_not be_valid
+    end
+
+    it  "is invalid without a last name" do
+      @user = User.new(:first_name => "adri", :last_name => nil, :email => "adriana@gmail.com", :password => "123456789", :password_confirmation => "123456789")
+      expect(@user).to_not be_valid
+    end
+
+    it  "is invalid without an email" do
+      @user = User.new(:first_name => 'Adri', :last_name => "Ana", :email => nil, :password => "123456789", :password_confirmation => "123456789")
+      expect(@user).to_not be_valid
+    end
+
+    it  "is invalid without a password" do
+      @user = User.new(:first_name => 'Adri', :last_name => "Ana", :email => "adriana@gmail.com", :password => nil, :password_confirmation => "123456789")
+      expect(@user).to_not be_valid
+    end
+
+    it  "is invalid without a confirmation password" do
+      @user = User.new(:first_name => 'Adri', :last_name => "Ana", :email => "adriana@gmail.com", :password => "123456789", :password_confirmation => nil)
+      expect(@user).to_not be_valid
+    end
   end
 
   describe '.authenticate_with_credentials' do
